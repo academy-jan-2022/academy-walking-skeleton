a1131df7f37f97748d8c104aef6e38445ba86e9c
Brian Hoang <brian.hd.hoang@codurance.com>
2022-03-01 16:36:46 +0000

    category repository dalesbred

--- simpleWebservice/src/main/java/com/kata/SimpleWebservice/Category/Model/CategoryRepository.java
+++ simpleWebservice/src/main/java/com/kata/SimpleWebservice/Category/Model/CategoryRepository.java
@@ -4,6 +4,9 @@ package com.kata.SimpleWebservice.Category.Model;
 import com.kata.SimpleWebservice.Category.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.List;
 
+public interface CategoryRepository {
+    void save(Category category);
+    List<Category> findAll();
 }
