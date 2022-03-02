05d17eaeaf706eaab92cef46ef2e95d30650bc5d
iagardi <garbigz@gmail.com>
2022-03-02 16:54:55 +0000

    looks good but doesn't work

--- simpleWebservice/src/main/java/com/kata/SimpleWebservice/SimpleWebserviceApplication.java
+++ simpleWebservice/src/main/java/com/kata/SimpleWebservice/SimpleWebserviceApplication.java
@@ -3,12 +3,24 @@ package com.kata.SimpleWebservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Duration;
+
 @SpringBootApplication
+@EnableCaching
 public class SimpleWebserviceApplication {
 
 	@Autowired
@@ -29,6 +41,22 @@ public class SimpleWebserviceApplication {
 				registry.addMapping("/allCategories").allowedOrigins(corsUrl);
 			}
 		};
+	}
+
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig().disableKeyPrefix()
+				.entryTtl(Duration.ofMinutes(1)).disableCachingNullValues()
+				.serializeValuesWith(RedisSerializationContext
+						.SerializationPair
+						.fromSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class)));
+
+		return RedisCacheManager.builder(connectionFactory).cacheDefaults(config).build();
+	}
+
+	@Bean
+	public RedisConnectionFactory connectionFactory(){
+		return new JedisConnectionFactory();
 	}
 
 }
---
+++ simpleWebservice/src/test/java/com/kata/SimpleWebservice/Category/Controller/CategoryCacheShould.java
@@ -0,0 +1,72 @@
+package com.kata.SimpleWebservice.Category.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kata.SimpleWebservice.Category.Model.Category;
+import com.kata.SimpleWebservice.Category.Service.CategoryService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@EnableCaching
+@WebMvcTest(CategoryQueryController.class)
+public class CategoryCacheShould {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CategoryService categoryService;
+
+    @Autowired
+    RedisCacheManager redisCacheManager;
+
+    @BeforeEach
+    void wipe_redis() {
+        for (String name : redisCacheManager.getCacheNames()) {
+            redisCacheManager.getCache(name).clear();
+        }
+    }
+
+    @Test void
+    create_a_category_and_when_retrieving_it_cache_it() throws Exception {
+        Category cachedCategory = new Category("cachedNames", "cachedDescription", "cachedPicture");
+        Category uncachedCategory = new Category("uncachedNames", "uncachedDescription", "uncachedPicture");
+
+        when(categoryService.getAllCategories()).thenReturn(List.of(cachedCategory)).thenReturn(List.of(uncachedCategory));
+
+        var requestOneContent = mockMvc.perform(get("/allCategories"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        var requestTwoContent = mockMvc.perform(get("/allCategories"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        Assertions.assertEquals(objectMapper.writeValueAsString(List.of(cachedCategory)), requestTwoContent);
+        verify(categoryService, times(1)).getAllCategories();
+    }
+
+}
