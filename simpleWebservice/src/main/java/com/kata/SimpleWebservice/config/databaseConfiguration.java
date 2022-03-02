88613b4e419cc775bc06915487a1ffa303616e2c
oawood93 <oa.wood@outlook.com>
2022-03-02 09:38:21 +0000

    Application configuration for dalesbred

--- simpleWebservice/src/main/java/com/kata/SimpleWebservice/config/databaseConfiguration.java
+++ simpleWebservice/src/main/java/com/kata/SimpleWebservice/config/databaseConfiguration.java
@@ -3,26 +3,24 @@ package com.kata.SimpleWebservice.config;
 import org.dalesbred.Database;
 import org.dalesbred.integration.spring.DalesbredConfigurationSupport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.datasource.lookup.DataSourceLookup;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
 import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class databaseConfiguration extends DalesbredConfigurationSupport {
 
-//     @Bean
-//    public DataSource getDataSource() {
-//        String url = "jdbc:postgresql://localhost:5432/postgresdb?user=postgres&password=postgres";
-//        return new JndiDataSourceLookup().getDataSource(url);
-//    }
-
 
     @Bean
-    public Database dalesbredDatabase() {
-        String url = "jdbc:postgresql://localhost:5432/postgresdb";
-        return Database.forUrlAndCredentials(url, "postgres", "postgres");
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
     }
-
 }
